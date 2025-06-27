bc7e6840fe17d55711e8a8b506385801d77a82d3
joaotiso09@gmail.com <joaotiso09@gmail.comgit config --global user.name joaogit status>
2025-06-27 13:23:31 -0300

    Resolve conflitos ulti

--- codeJ/Const.py
+++ codeJ/Const.py
@@ -1,2 +1,13 @@
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
+
+#C
+COLOR_ORANGE = (200, 80, 0)
+COLOR_BLACK = (0, 0, 0)
+
+#M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P COOPERATIVE',
+               'NEW GAME 2P VERSUS',
+               'SCORE',
+               'EXIT')
--- codeJ/menu.py
+++ codeJ/menu.py
@@ -2,6 +2,8 @@ from pygame import Rect, Surface
 from pygame.font import Font
 import pygame.image
 
+from codeJ.Const import COLOR_BLACK, COLOR_ORANGE, MENU_OPTION, WIN_WIDTH
+
 class Menu:
     def __init__(self, window):
         self.window = window
@@ -15,6 +17,12 @@ class Menu:
 
          while True:
              self.window.blit(source=self.surf, dest=self.rect)
+             self.menu_text( text_size=50, text='MONTAIN', text_color=COLOR_ORANGE, text_center_pos=((WIN_WIDTH // 2), 70))
+             self.menu_text( text_size=50, text='HIGHSCORE', text_color=COLOR_ORANGE, text_center_pos=((WIN_WIDTH // 2), 120))
+
+             for i in range(len(MENU_OPTION)):
+                 self.menu_text(text_size=20, text=MENU_OPTION[i], text_color=COLOR_BLACK, text_center_pos=((WIN_WIDTH // 2), 200 + (i * 27)))
+
              pygame.display.flip()
              
              for event in pygame.event.get():
@@ -23,7 +31,7 @@ class Menu:
                      quit()
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
-     text_font: Font = pygame.Font.SysFont(name="Lucida Sans Typewriter", size=text_size)
-     text_surf: Surface = text_font.render(text, antialias=True, color=text_color).convert_alpha()
+     text_font: Font = pygame.font.SysFont(name="Impact", size=text_size)
+     text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
      text_rect: Rect = text_surf.get_rect(center=text_center_pos)
      self.window.blit(source=text_surf, dest=text_rect)
